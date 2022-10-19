f3996127627d3d27b4e9e6908034ae4e390e9fc5
danuwardanaaa <danuwardana@student.telkomuniversity.ac.id>
2022-10-19 21:17:43 +0700

    MEMEKKKKK

---
+++ coba.py
@@ -0,0 +1,13 @@
+print("Program Perhitungan")
+a = 12
+b = 8
+bagi    = kali 
+kurang  = bagi
+kali    = kurang
+tambah  = tambah
+print(f'''
+kali    : {a*b}
+bagi    : {a*b}
+kurang  : {a*b}
+tambah  : {a*b}
+''')
